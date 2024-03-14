a60fb7500c35f36709ae2d8bd8478a43128ab7ad
gracielaram <chela513805@gmail.com>
2024-03-14 11:30:33 -0700

    grep commands

--- lab8.sh
+++ lab8.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 python3 ./lab9.py
 
-output=./lab9.py
+output= 'grep "class"./lab9.py
 
-if [ INFILE == class 
+if [ -z output]
+then
+    echo " Has the function $output "
+elso 
+    echo " Do not contain class funtion " 
 
 
 
