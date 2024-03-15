fe7a2381291b4ec2760671a517c42cf7bf020c1d
gracielaram <chela513805@gmail.com>
2024-03-15 11:41:49 -0700

    change code

--- lab8.sh
+++ lab8.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 python3 ./lab9.py
+grep -n 'class\|Application\|import\|greeting' lab9.py
 
-output= 'grep "class"./lab9.py
+#while read -r output
 
-if [ -z output]
-then
-    echo " Has the function $output "
-elso 
-    echo " Do not contain class funtion " 
+####if [ output ]
+#then
+ #  # echo "File does contain $output"
+##else 
+   # echo " Does not contain class funtion " 
 
-
-
-
-
-
-
-
-""while read -r LINE
-do 
-    echo "$LINE"
-done <  $INFILE""
+#fi
+# ##   echo LINE done <  $INFILE
 
 
