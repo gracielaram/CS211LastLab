86cf58d1dfff15111e92e72b20af9a036ec33ece
gracielaram <chela513805@gmail.com>
2024-03-17 13:08:05 -0700

    Adding more to grep

--- lab8.sh
+++ lab8.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 python3 $1
-grep -n 'class\|App\|import\|greeting\|1\|main' $1
-
-
-
-
-##while read -r output
-
-#if [-n x ]
-#then
- # echo "File does contain $output"
-#else 
- #   echo " Does not contain class funtion " 
-#fi
- #  echo LINE done <  $1
-
+grep -n 'class\|App\|import\|greeting\|1\|main\|def\|275x250' $1
 
