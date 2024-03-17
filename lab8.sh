79c691eee4d16dea7650597b6708d2ff447cf2b8
gracielaram <chela513805@gmail.com>
2024-03-17 12:49:34 -0700

    Update

--- lab8.sh
+++ lab8.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 python3 $1
-grep -n 'class\|App\|import\|greeting' $1
+grep -n 'class\|App\|import\|greeting\|1\|main' $1
 
-#while read -r output
 
-####if [ output ]
+
+
+##while read -r output
+
+#if [-n x ]
 #then
- #  # echo "File does contain $output"
-##else 
-   # echo " Does not contain class funtion " 
-
+ # echo "File does contain $output"
+#else 
+ #   echo " Does not contain class funtion " 
 #fi
-# ##   echo LINE done <  $INFILE
+ #  echo LINE done <  $1
 
 
