d143bcccd7025772c87ec4d67108054c6234ead6
gracielaram <chela513805@gmail.com>
2024-03-17 22:24:02 -0700

    If

--- lab8.sh
+++ lab8.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 python3 $1
+if [ $? -eq 0 ]
+then
+    echo "Successfully Ran"
+else 
+    echo "Could not run"
+fi
+
 grep -n 'class\|App\|import\|greeting\|1\|main\|def\|275x250' $1
-file=$1
-while read -r LINE
-do 
-    printf '%s\n' "$LINE"
-done < $file
 
 
