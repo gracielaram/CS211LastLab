5f45aba4bad5ed794a64b07f8b285270887d8d6e
gracielaram <chela513805@gmail.com>
2024-03-17 22:25:41 -0700

    echo update

--- lab8.sh
+++ lab8.sh
@@ -2,9 +2,9 @@
 python3 $1
 if [ $? -eq 0 ]
 then
-    echo "Successfully Ran"
+    echo "Successfully Ran!!!"
 else 
-    echo "Could not run"
+    echo "Could not run an error was encountered"
 fi
 
 grep -n 'class\|App\|import\|greeting\|1\|main\|def\|275x250' $1
